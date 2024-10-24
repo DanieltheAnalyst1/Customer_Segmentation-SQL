7d98d72e60e8132064831bc50d9d44a78ed2542b
Daniel Adesanya <146339509+DanieltheAnalyst1@users.noreply.github.com>
2024-10-24 23:19:16 +0100

    Add files via upload

---
+++ Customer Segmentation.sql
@@ -0,0 +1,36 @@
+-- create database Customer_Segmentation;
+-- select * from dataset_superstore;
+
+-- RFM analysis
+WITH rfm AS (
+    SELECT Customer_ID,
+           MAX(Order_Date) AS last_purchase,
+           COUNT(Order_ID) AS frequency,
+           SUM(Sales) AS monetary
+    FROM dataset_superstore
+    GROUP BY Customer_ID
+)
+SELECT Customer_ID,
+       DATEDIFF(CURDATE(), last_purchase) AS recency,
+       frequency,
+       monetary
+FROM rfm
+ORDER BY recency, frequency DESC, monetary DESC;
+
+-- High-value customers
+WITH rfm AS (
+    SELECT Customer_ID,
+           MAX(Order_Date) AS last_purchase,
+           COUNT(Order_ID) AS frequency,
+           SUM(Sales) AS monetary
+    FROM dataset_superstore
+    GROUP BY Customer_ID
+)
+SELECT Customer_ID,
+       DATEDIFF(CURDATE(), last_purchase) AS recency,
+       Customer_ID,
+       frequency,
+       monetary
+FROM rfm
+WHERE monetary > (SELECT AVG(monetary) FROM rfm)
+ORDER BY monetary DESC;
